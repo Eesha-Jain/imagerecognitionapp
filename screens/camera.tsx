8e969063701638dc806bc68aca564570037def77
HiiDeff <68827992+Eesha-Jain@users.noreply.github.com>
2021-11-28 20:03:12 -0800

    Finished image recognition app

--- screens/camera.tsx
+++ screens/camera.tsx
@@ -1,68 +1,54 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Dimensions, Alert, StyleSheet, ActivityIndicator } from 'react-native';
-import { RNCamera } from 'react-native-camera';
-import CaptureButton from './capturebutton.tsx';
-import styles from './styles/style';
+import { Camera } from 'expo-camera';
+let camera: Camera;
+import CaptureButton from './capturebutton';
 
-export default function Camera() {
-  const [identifedAs, setIdentifedAs] = useState("");
-  const [loading, setLoading] = useState(false);
+export default function CameraPage() {
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    startCamera();
-  });
+	const takePicture = async () => {
+		if (camera) {
+			camera.pausePreview();
+			setLoading(true);
+			const data = await camera.takePictureAsync(options={base64:true});
+		}
+	}
 
-  const startCamera = async () => {
-    const {status} = await Camera.requestCameraPermissionsAsync();
-    if (status !== 'granted') {
-      Alert.alert('Access denied');
-    }
-  }
+	const identifyImage = (imageData) => {
+		const Clarifai = require('clarifai');
+		const app = new Clarifai.App({ apiKey: 'f349ed37dec446c6b8837a3384a73d00' });
 
-  const takePicture = async function() {
-      if (this.camera) {
-        // Pause the camera's preview
-        this.camera.pausePreview();
-        // Set the activity indicator
-        setLoading(true);
-        // Set options
-        const options = {
-            base64: true
-        };
-        // Get the base64 version of the image
-        const data = await this.camera.takePictureAsync(options)
-        // Get the identified image
-        this.identifyImage(data.base64);
-      }
-  }
+		app.models.predict(Clarifai.GENERAL_MODEL, {base64: imageData})
+			.then((response) => {displayAnswer(response.outputs[0].data.concepts[0].name);})
+			.catch((err) => alert(err));
+	}
 
-  function identifyImage(imageData) {
-    // Initialise Clarifai api
-    const Clarifai = require('clarifai');
-    const app = new Clarifai.App({
-        apiKey: 'RECOGNITION'
-    });
-    // Identify the image
-    app.models.predict(Clarifai.GENERAL_MODEL, {base64: imageData})
-    .then((response) => this.displayAnswer(response.outputs[0].data.concepts[0].name)
-    .catch((err) => alert(err))
-    );
-  }
+	const displayAnswer = (identifiedImage) => {
+		setLoading(false);
+		Alert.alert(identifiedImage, '', { cancelable: false } )
+		camera.resumePreview();
+	}
 
-  function displayAnswer(identifiedImage) {
-    // Dismiss the acitivty indicator
-    setLoading(false);
-    setIdentifedAs(identifyImage);
-    // Show an alert with the answer on
-    Alert.alert(identifedAs, '', { cancelable: false })
-    // Resume the preview
-    this.camera.resumePreview();
-  }
-
-  return (
-    <RNCamera ref={ref => {this.camera = ref;}} style={styles.preview}>
-      <ActivityIndicator size="large" style={styles.loadingIndicator} color="#fff" animating={loading}/>
-      <CaptureButton buttonDisabled={loading} onClick={takePicture}/>
-    </RNCamera>
-  );
+	return (
+		<Camera ref={(r) => { camera = r }} style={styles.preview} type={Camera.Constants.Type.back} flashMode='off'>
+			<ActivityIndicator size="large" style={styles.loadingIndicator} color="#fff" animating={loading}/>
+			<CaptureButton buttonDisabled={loading} onClick={(data) => {takePicture(data);}}/>
+		</Camera>
+	);
 }
+
+const styles = StyleSheet.create({
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width,
+    },
+    loadingIndicator: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
--- screens/capturebutton.tsx
+++ screens/capturebutton.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React  from 'react';
 import { StyleSheet, Button, TouchableHighlight } from 'react-native';
-import styles from './styles/style';
 
-export default function Camera() {
-  const [identifedAs, setIdentifedAs] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  return (
-    <TouchableHighlight style={styles.captureButton} disabled={this.props.buttonDisabled}>
-      <Button onPress={this.props.onClick} disabled={this.props.buttonDisabled} title="Capture" accessibilityLabel="Learn more about this button"/>
-    </TouchableHighlight>
-  );
+export default function CaptureButton(props) {
+    return (
+        <TouchableHighlight style={styles.captureButton} disabled={props.buttonDisabled}>
+            <Button onPress={props.onClick} disabled={props.buttonDisabled} title="Capture" accessibilityLabel="Learn more about this button"/>
+        </TouchableHighlight>
+    );
 }
+const styles = StyleSheet.create({
+    captureButton: {
+        marginBottom:30,
+        width:160,
+        borderRadius:10,
+        backgroundColor: "white"
+    }
+});
